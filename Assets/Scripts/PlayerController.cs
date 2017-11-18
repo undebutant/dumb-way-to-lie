77ad6f8197f93907ba5951538d2590ba129a32a0
Cyhzault <Cyhzault@gmail.com>
2017-11-18 04:06:20 +0100

    Ajout de la physique (Unity) et des controllers manette + clavier

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+
+    #region playerSettings
+    [SerializeField]
+    [Tooltip("Facteur de vitesse sur l'axe X du joueur.")]
+    private float playerSpeedFactor = 1;
+    [SerializeField]
+    [Tooltip("Hauteur de saut atteinte par le joueur.Alors non plus maintenant.")]
+    private float jumpHeight = 5;
+
+    #endregion
+
+    Rigidbody2D r;
+    
+    Vector3 acceleration;
+
+    // Use this for initialization
+    void Start () {
+        acceleration = Vector3.zero;
+        r = this.GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (InputController.getJump())
+        {
+            this.r.AddForce(Vector2.up * jumpHeight);
+        }
+
+        acceleration.x = InputController.getXAxis() * playerSpeedFactor*Time.deltaTime;
+        this.transform.Translate(acceleration);
+	}
+}
